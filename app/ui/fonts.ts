5f4e76eaba64cc18e44a9f5a45ae2f6e2b5c7648
maborri <matiasdborri@gmail.com>
2025-01-22 19:47:44 -0300

    fix imports

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Smooch_Sans, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const smoochSans = Smooch_Sans({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
